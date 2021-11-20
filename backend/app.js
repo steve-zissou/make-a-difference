f08065c8bea10d3b2b13c91f44bba170266a3994
Steve Kitchen <stephen.kitchen@eigentech.com>
2021-11-20 11:32:11 +0000

    Enable cors and json bodies

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
+var cors = require('cors');
 
 // routes
 const books = require('./routes/api/books');
 
 const app = express();
+app.use(express.json());
 
 // Connect Database
 connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
