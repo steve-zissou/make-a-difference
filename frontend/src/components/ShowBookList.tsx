9bb5cf82ddd58e0fc5dcfb386b126726bd1dec51
Steve Kitchen <stephen.kitchen@eigentech.com>
2021-11-20 20:54:00 +0000

    Removed unused imports

--- frontend/src/components/ShowBookList.tsx
+++ frontend/src/components/ShowBookList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../App.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
